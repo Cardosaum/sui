84f8fa2518ef04a423cc7317f21a7802c8059a50
Andrew Burnie <apburnie@hotmail.co.uk>
2022-09-26 14:26:09 +0100

    changes openbody to be a flex

--- explorer/client/src/components/ownedobjects/views/OwnedCoinView.tsx
+++ explorer/client/src/components/ownedobjects/views/OwnedCoinView.tsx
@@ -56,30 +56,31 @@ function SingleCoinView({
                 </div>
                 <div />
             </div>
-            <div className={styles.openbody}>
-                {isOpen &&
-                    subObjList.map((subObj, index) => (
-                        <React.Fragment key={index}>
-                            <div className={styles.objectid}>
-                                <div />
-                                <div>Object ID</div>
-                                <div className={styles.oneline}>
+            {isOpen && (
+                <div className={styles.openbody}>
+                    {subObjList.map((subObj, index) => (
+                        <div key={index} className={styles.singlecoin}>
+                            <div className={styles.openrow}>
+                                <div className={styles.label}>Object ID</div>
+                                <div
+                                    className={`${styles.oneline} ${styles.value}`}
+                                >
                                     <Longtext
                                         text={subObj.id}
                                         category="objects"
                                     />
                                 </div>
-                                <div />
                             </div>
-                            <div className={styles.balance}>
-                                <div />
-                                <div>Balance</div>
-                                <div>{subObj.balance?.toString()}</div>
-                                <div />
+                            <div className={styles.openrow}>
+                                <div className={styles.label}>Balance</div>
+                                <div className={styles.value}>
+                                    {subObj.balance?.toString()}
+                                </div>
                             </div>
-                        </React.Fragment>
+                        </div>
                     ))}
-            </div>
+                </div>
+            )}
         </div>
     );
 }
